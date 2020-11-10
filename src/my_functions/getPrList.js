c03be3c6ab84615688b0a303cd75cc9ffeb570be
Jon E. Eguiluz <viroide@gmail.com>
2020-11-10 10:38:01 +0100

    Small refactor Add some semantics

--- src/my_functions/getPrList.js
+++ src/my_functions/getPrList.js
@@ -3,20 +3,20 @@ const { Octokit } = require("@octokit/core");
 const octokit = new Octokit({ auth: process.env.TOKEN });
 
 exports.handler = async () => {
-    const response = await octokit.request("GET /search/issues", {
+    const prList = await octokit.request("GET /search/issues", {
         q: "is:open+is:pr+user:Gluckgames+archived:false+author:viroide"
     });
-    const prList = await Promise.all(response.data.items.map(async pr => {
-        const r2 = await octokit.request("GET " + pr.pull_request.url, {});
-        const r3 = await octokit.request("GET " + r2.data.statuses_url, {});
+    const prResume = await Promise.all(prList.data.items.map(async pr => {
+        const prInfo = await octokit.request("GET " + pr.pull_request.url, {});
+        const prStatus = await octokit.request("GET " + prInfo.data.statuses_url, {});
         return {
             title: pr.title,
             comments: pr.comments,
-            state: r3.data[0].state
+            state: prStatus.data[0].state
         };
     }));
     return {
         statusCode: 200,
-        body: JSON.stringify(prList)
+        body: JSON.stringify(prResume)
     };
 };
