33b898397a326dbddc85862eb2159bf84f2ab9d1
Jon E. Eguiluz <viroide@gmail.com>
2020-11-10 11:00:53 +0100

    Return the PR id too

--- src/my_functions/getPrList.js
+++ src/my_functions/getPrList.js
@@ -3,13 +3,13 @@ const { Octokit } = require("@octokit/core");
 const octokit = new Octokit({ auth: process.env.TOKEN });
 
 exports.handler = async () => {
-    const prList = await octokit.request("GET /search/issues", {
-        q: "is:open+is:pr+user:Gluckgames+archived:false+author:viroide"
-    });
+    const q = "is:open+is:pr+user:Gluckgames+archived:false+author:viroide";
+    const prList = await octokit.request("GET /search/issues", { q });
     const prResume = await Promise.all(prList.data.items.map(async pr => {
         const prInfo = await octokit.request("GET " + pr.pull_request.url, {});
         const prStatus = await octokit.request("GET " + prInfo.data.statuses_url, {});
         return {
+            id: pr.id,
             title: pr.title,
             comments: pr.comments,
             state: prStatus.data[0].state
