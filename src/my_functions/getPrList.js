7c6e0da110c0c335685e52b2e59230bb4bc16865
Jon E. Eguiluz <viroide@gmail.com>
2024-03-25 21:03:45 +0100

    Re-new libs

--- src/my_functions/getPrList.js
+++ src/my_functions/getPrList.js
@@ -1,27 +1,28 @@
-const { Octokit } = require("@octokit/core");
+const { Octokit } = require("octokit");
 
 const octokit = new Octokit({ auth: process.env.TOKEN });
 
 exports.handler = async () => {
-    const q = "is:open+is:pr+user:Gluckgames+archived:false+author:viroide";
-    const prList = await octokit.request("GET /search/issues", { q });
-    const prResume = await Promise.all(prList.data.items.map(async pr => {
-        const prInfo = await octokit.request("GET " + pr.pull_request.url, {});
-        const prStatus = await octokit.request("GET " + prInfo.data.statuses_url, {});
-        const state = prStatus.data.length > 0? prStatus.data[0].state : "unknown";
-        return {
-            id: pr.id,
-            title: pr.title,
-            comments: pr.comments,
-            state
-        };
-    }));
+  const q = "is:open+is:pr+user:Lingokids+archived:false+assignee:viroide";
+  const prList = await octokit.request("GET /search/issues", { q });
+  const prResume = await Promise.all(prList.data.items.map(async pr => {
+    const prInfo = await octokit.request("GET " + pr.pull_request.url, {});
+    const prStatus = await octokit.request("GET " + prInfo.data.statuses_url, {});
+    const state = prStatus.data.length > 0 ? prStatus.data[0].state : "unknown";
     return {
-        statusCode: 200,
-        headers: {
-            'Allow': 'GET',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(prResume)
+      id: pr.id,
+      title: pr.title,
+      comments: pr.comments,
+      state
     };
+  }));
+  return {
+    statusCode: 200,
+    headers: {
+      'Allow': 'GET',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(prResume)
+  };
 };
+
