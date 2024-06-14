1cc69f08ac56555de74540b905e4fa4418572425
Jon E. Eguiluz <viroide@gmail.com>
2024-06-14 15:44:04 +0200

    Inproves the code and the result

--- src/my_functions/getPrList.js
+++ src/my_functions/getPrList.js
@@ -5,33 +5,42 @@ const octokit = new Octokit({ auth: process.env.TOKEN });
 exports.handler = async () => {
   const q = "is:open+is:pr+user:Lingokids+archived:false+assignee:viroide";
   const prList = await octokit.request("GET /search/issues", { q });
-  const prResume = await Promise.all(prList.data.items.map(async pr => {
-    const prInfo = await octokit.request("GET " + pr.pull_request.url, {});
+  const prResume = await Promise.all(
+    prList.data.items.map(async (pr) => {
+      const prInfo = await octokit.request("GET " + pr.pull_request.url, {});
 
-    let state = "";
-    prInfo.data.draft ? "unknown" : "success"
-    if (prInfo.data.draft) {
-      state = "unknown";
-    } else if (prInfo.data.mergeable) {
-      state = "success";
-    } else {
-      state = "error";
-    }
-    return {
-      id: pr.id,
-      title: pr.title,
-      comments: pr.comments - 1,
-      state
-    };
-  }));
+      // GREEN: success & 0 comments
+      // BLUE:  success & 1+ comments
+      // MAGENTA: unknown
+      // RED: error
+      // YELLOW: pending
+      // CYAN: ??
+      //
+      let state = "";
+      if (prInfo.data.draft) {
+        state = "unknown";
+      } else if (prInfo.data.mergeable) {
+        state = "success";
+      } else {
+        state = "error";
+      }
+      console.log(prInfo.data);
+      return {
+        id: pr.id,
+        title: pr.title,
+        comments: prInfo.data.review_comments,
+        state,
+      };
+    }),
+  );
 
+  console.log(prResume);
   return {
     statusCode: 200,
     headers: {
-      'Allow': 'GET',
-      'Content-Type': 'application/json',
+      Allow: "GET",
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(prResume)
+    body: JSON.stringify(prResume),
   };
 };
-
